c74a0bde25858b017814861aae15c65d714e30a9
msoler18 <msoler735@gmail.com>
2018-06-07 16:58:19 -0500

    Add comments and operators

---
+++ basics.js
@@ -0,0 +1,63 @@
+//Comentarios
+//Una línea
+  //Este es un comentario de una linea
+//Multiples líneas
+  /*
+  Esta es una línea
+  Esta es otra línea
+  Etc..
+  */
+//JSDoc
+ /**
+ *Represents a book
+ *@constructor
+ *@param {string} title - The title of the book
+ *@param {string} author - The author of the book
+ */
+
+//Nombres de variables
+  
+  // No usar espacios
+  var con espacios = 1;
+  // No usar un número adelante
+  var 1numero = 1;
+  //Evitar simbolos y snake case
+  var con_guiones_bajos = 1;
+  var dame$ = 1;
+  // Usar CamelCase
+  var myName = "Miguel";
+
+/*
+Si estas realizando cambios en un código que sigue una pauta distinta a cameCase.
+¡Síguelo!
+La coherencia en el código es más importante, recuérdalo.
+*/
+
+//Operadores
+  // = Asignación
+  var x = x;
+  // += Suma
+  var x = 1;
+  var y = 2;
+  x += y //x = x + y
+  // -= Resta
+  var x = 2;
+  var y = 3;
+  x -= y // x = x - y
+  // *= Resta
+  var x = 2;
+  var y = 3;
+  x *= y // x = x * y
+  // /= Resta
+  var x = 2;
+  var y = 3;
+  x /= y // x = x / y  
+
+//Operadores de comparación
+var mayorQue = 100 > 10;
+var menorQue = 10 < 100;
+var mayorIgual = 100 >= 10;
+var menorIgual = 10 <= 100;
+var igual = 10 == 10;
+var igualTotalmente = 10 === 10; // Ojo! Usamos también === Compara valor y tipo
+var noIgual = 100 != 10;  
